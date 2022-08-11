89e57ae4a13678a1331fc663252c884938c42a16
marurevi <maryrev@hotmail.com>
2022-08-10 22:35:19 -0300

    Add CRUD methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', class_name: 'Post'
-  has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
-  has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
