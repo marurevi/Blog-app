9c896deebd599a262b3b34cea0819ef825f6b18f
marurevi <maryrev@hotmail.com>
2022-08-12 00:28:54 -0300

    Write TDD schema to test Like class

---
+++ spec/like_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Like do
+  fcontext 'When testing the Like class' do
+    it 'should contain an author'
+    it 'should contain a post'
+    it 'should update likes counter'
+  end
+end
