b8b6ed0f11fe6cac4297033c78524b5f629d4f84
marurevi <maryrev@hotmail.com>
2022-08-15 13:47:40 -0300

    Generate controller for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+    def index
+        @posts = User.find(params[:user_id]).posts
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+        
+end
