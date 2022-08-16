8d2bca04361e1e6894373e2fbbaf2f02956b9afe
marurevi <maryrev@hotmail.com>
2022-08-16 19:48:05 -0300

    Create Users and Posts tests

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,24 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  # let(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Canada.') }
+  let(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Canada.') }
 
-  fdescribe 'GET /index' do
+  describe 'GET /index' do
     before do
-      get '/users/index'
+      get users_path
     end
 
     it 'should render a successful response' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should assert which template or partials being rendered'
-    # expect(response).to render_template(:index)
+    it 'should assert which template or partials being rendered' do
+      expect(response).to render_template(:index)
+    end
 
-    it 'should assert the content of the response body'
-    # expect(response.body).to include("<h1>Hello World</h1>")
+    it 'should assert the content of the response body' do
+      expect(response.body).to include('<h3>Names of all the Users</h3>')
+    end
   end
+
   describe 'GET /show' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      get "/users/#{user.id}"
+    end
+
+    it 'should render a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should assert which template or partials being rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should assert the content of the response body' do
+      expect(response.body).to include('<h3>Names of the required author is:</h3>')
+    end
   end
 end
