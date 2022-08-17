76bbe6d1e89c7988401d3140a963777851bdeb3a
marurevi <maryrev@hotmail.com>
2022-08-17 20:29:23 -0300

    Add post index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @comments = Comment.where(post_id: @posts.ids)
   end
 
   def show
     @post = Post.find(params[:id])
+
   end
 end
