9370ed166dc84a04d3caea032041d61832b71dd6
marurevi <maryrev@hotmail.com>
2022-08-18 03:28:37 -0300

    Fix show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-    @comments = Comment.where(post_id: @posts.ids)
   end
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.order(created_at: :desc)
   end
 end
