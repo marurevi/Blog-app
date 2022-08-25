721af28a20409af6bf26e1d192e8a318907f01bc
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-25 14:10:02 -0600

    Give user access in ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,16 +8,12 @@ class Ability
     # Handle the case where we don't have a current_user i.e. the user is a guest
     user ||= User.new
 
-    # Define a few sample abilities
-    can    :manage , Article
-    cannot :manage , Comment
-    can    :read   , Tag , released: true
-
-    return unless user.present?  # additional permissions for logged in users (they can read their own posts)
-    can :read, Post, user: user
-
-    return unless user.admin?  # additional permissions for administrators
-    can :read, Post
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Post, author_id: user.id
+    end
   end
   
 end
